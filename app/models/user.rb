808a1b1cc56865d8f4c1f0a521359b8cf4360514
Sam <logos50907@gmail.com>
2013-05-29 15:43:57 -0400

    Finish Exercise 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,6 @@ class User < ActiveRecord::Base
     uniqueness: { case_sentitive: false }
                     
   validates :password, 
-    presence: true,
     length: { minimum: 6 }
                        
   validates :password_confirmation, 
